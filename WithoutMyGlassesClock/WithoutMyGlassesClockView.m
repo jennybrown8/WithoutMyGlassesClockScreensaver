243307c7f4f01a029f553ab3e7eba92d6bf9c300
Jenny Brown <jennybrown8@gmail.com>
2018-04-16 16:30:59 -0500

    Draw clock as large as possible within screen bounds

--- WithoutMyGlassesClock/WithoutMyGlassesClockView.m
+++ WithoutMyGlassesClock/WithoutMyGlassesClockView.m
@@ -34,11 +34,73 @@
     [super drawRect:rect];
 }
 
-- (void)animateOneFrame
-{
-    return;
+// Style the clock text to fill the available space but not run over in either direction.
+static NSMutableDictionary * createFontStylingDictionary(float textsize) {
+    NSFont* font = [NSFont fontWithName:@"Times New Roman Bold" size:textsize];
+    
+    NSMutableParagraphStyle *style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+    style.alignment = NSTextAlignmentCenter;
+    NSMutableDictionary *myDictionary = [NSMutableDictionary dictionary];
+    [myDictionary setObject:style  forKey:NSParagraphStyleAttributeName];
+    [myDictionary setObject:font  forKey:NSFontAttributeName];
+    [myDictionary setObject:[NSColor redColor] forKey:NSForegroundColorAttributeName];
+
+    // about 5% kerning tightening
+    NSNumber *kerning = [NSNumber numberWithFloat:(-1.0 * 0.05 * textsize)];
+    [myDictionary setObject:kerning forKey:NSKernAttributeName];
+    return myDictionary;
 }
 
+static float calculatePointSizeToFillScreen(CGSize boundingSize, float screenHeight, float screenWidth) {
+    CGRect labelRect;
+    float priorPointSize = 0.0;
+    float pointsize = 12.0;
+    float margin = 20;
+    
+    // Todo: Is there any method other than trial and error to figure out the right sizing?
+    while ( labelRect.size.height < (screenHeight - margin) && labelRect.size.width < (screenWidth - margin)) {
+        priorPointSize = pointsize;
+        labelRect = [@"12:59"
+                      boundingRectWithSize:boundingSize
+                      options:NSStringDrawingUsesFontLeading
+                      attributes:createFontStylingDictionary(pointsize)
+                      context:nil];
+        pointsize += 10;
+    }
+    return priorPointSize;
+}
+
+- (void)animateOneFrame
+{
+    // How big of a screen are we working with?
+    NSSize size = [self bounds].size;
+    NSRect screenrect;
+    screenrect.size = NSMakeSize(size.width, size.height);
+    
+    // Get and format the current time
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"h:mm"];
+    NSDate *currentDate = [NSDate date];
+    NSString *dateString = [formatter stringFromDate:currentDate];
+    
+    // Draw a rectangle background to clear any prior drawing
+    NSRect bgrect;
+    bgrect.size = NSMakeSize(size.width, size.height);
+    bgrect.origin = CGPointMake(0, 0);
+    NSBezierPath *path = [NSBezierPath bezierPathWithRect:bgrect];
+    NSColor *color = [NSColor colorWithCalibratedRed:0.0
+                                               green:0.0
+                                                blue:0.0
+                                               alpha:255.0];
+    [color set];
+    [path fill];
+    
+    // Need to figure out how big to make the clock text.
+    float largestPointSize = calculatePointSizeToFillScreen(self.bounds.size, size.height, size.width);
+    
+    // Draw the time as text on the screen rectangle using the styling attributes specified in dict
+    [dateString drawInRect:screenrect withAttributes:createFontStylingDictionary(largestPointSize)];
+}
 - (BOOL)hasConfigureSheet
 {
     return NO;
