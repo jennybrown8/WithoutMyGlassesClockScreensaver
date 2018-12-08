123c0ff92c44a06f91090774597a228ba0d3e97e
Jenny Brown <jennybrown8@gmail.com>
2018-12-08 11:26:46 -0600

    Fixes for Mojave, plus dim the red a bit

--- WithoutMyGlassesClock/WithoutMyGlassesClockView.m
+++ WithoutMyGlassesClock/WithoutMyGlassesClockView.m
@@ -29,48 +29,9 @@
     [super stopAnimation];
 }
 
+// this code used to be in animateOneFrame but got moved due
+// to the bug: https://github.com/lionheart/openradar-mirror/issues/20659
 - (void)drawRect:(NSRect)rect
-{
-    [super drawRect:rect];
-}
-
-// Style the clock text to fill the available space but not run over in either direction.
-static NSMutableDictionary * createFontStylingDictionary(float textsize) {
-    NSFont* font = [NSFont fontWithName:@"Times New Roman Bold" size:textsize];
-    
-    NSMutableParagraphStyle *style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
-    style.alignment = NSTextAlignmentCenter;
-    NSMutableDictionary *myDictionary = [NSMutableDictionary dictionary];
-    [myDictionary setObject:style  forKey:NSParagraphStyleAttributeName];
-    [myDictionary setObject:font  forKey:NSFontAttributeName];
-    [myDictionary setObject:[NSColor redColor] forKey:NSForegroundColorAttributeName];
-
-    // about 5% kerning tightening
-    NSNumber *kerning = [NSNumber numberWithFloat:(-1.0 * 0.05 * textsize)];
-    [myDictionary setObject:kerning forKey:NSKernAttributeName];
-    return myDictionary;
-}
-
-static float calculatePointSizeToFillScreen(CGSize boundingSize, float screenHeight, float screenWidth) {
-    CGRect labelRect;
-    float priorPointSize = 0.0;
-    float pointsize = 12.0;
-    float margin = 20;
-    
-    // Todo: Is there any method other than trial and error to figure out the right sizing?
-    while ( labelRect.size.height < (screenHeight - margin) && labelRect.size.width < (screenWidth - margin)) {
-        priorPointSize = pointsize;
-        labelRect = [@"12:59"
-                      boundingRectWithSize:boundingSize
-                      options:NSStringDrawingUsesFontLeading
-                      attributes:createFontStylingDictionary(pointsize)
-                      context:nil];
-        pointsize += 10;
-    }
-    return priorPointSize;
-}
-
-- (void)animateOneFrame
 {
     // How big of a screen are we working with?
     NSSize size = [self bounds].size;
@@ -95,11 +56,58 @@ static float calculatePointSizeToFillScreen(CGSize boundingSize, float screenHei
     [color set];
     [path fill];
     
-    // Need to figure out how big to make the clock text.
-    float largestPointSize = calculatePointSizeToFillScreen(self.bounds.size, size.height, size.width);
-    
     // Draw the time as text on the screen rectangle using the styling attributes specified in dict
+    float largestPointSize = calculatePointSizeToFillScreen(self.bounds.size, size.height, size.width);
     [dateString drawInRect:screenrect withAttributes:createFontStylingDictionary(largestPointSize)];
+    
+    // if the above is slow or crashy, use this.
+    //[dateString drawInRect:screenrect withAttributes:createFontStylingDictionary(750)]; // was 750
+}
+
+// Style the clock text to fill the available space but not run over in either direction.
+static NSMutableDictionary * createFontStylingDictionary(float textsize) {
+    
+    // alpha 1.0 = solid, 0.0 = transparent.
+    NSColor *darkRedColor = [NSColor colorWithCalibratedRed:0.2 green:0.0 blue:0.0 alpha:1.0];
+    
+    // TODO: Configurable font choice
+    NSFont* font = [NSFont fontWithName:@"Times New Roman Bold" size:textsize];
+    
+    NSMutableParagraphStyle *style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+    style.alignment = NSTextAlignmentCenter;
+    NSMutableDictionary *myDictionary = [NSMutableDictionary dictionary];
+    [myDictionary setObject:style  forKey:NSParagraphStyleAttributeName];
+    [myDictionary setObject:font  forKey:NSFontAttributeName];
+    [myDictionary setObject:darkRedColor forKey:NSForegroundColorAttributeName];
+    
+    // about 5% kerning tightening reduces gaps and makes the numbers fit on screen better
+    NSNumber *kerning = [NSNumber numberWithFloat:(-1.0 * 0.05 * textsize)];
+    [myDictionary setObject:kerning forKey:NSKernAttributeName];
+    return myDictionary;
+}
+
+
+static float calculatePointSizeToFillScreen(CGSize boundingSize, float screenHeight, float screenWidth) {
+    CGRect labelRect;
+    float priorPointSize = 0.0;
+    float pointsize = 12.0;
+    float margin = 20;
+    
+    // Todo: Is there any method other than trial and error to figure out the right sizing?
+    while ( labelRect.size.height < (screenHeight - margin) && labelRect.size.width < (screenWidth - margin)) {
+        priorPointSize = pointsize;
+        labelRect = [@"12:59"
+                     boundingRectWithSize:boundingSize
+                     options:NSStringDrawingUsesFontLeading
+                     attributes:createFontStylingDictionary(pointsize)
+                     context:nil];
+        pointsize += 10;
+    }
+    return priorPointSize;
+}
+
+- (void)animateOneFrame
+{
 }
 - (BOOL)hasConfigureSheet
 {
